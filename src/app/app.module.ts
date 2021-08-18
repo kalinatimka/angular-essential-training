43ac64f17fcb11d71931628f834b7c842e2f25ab
Tsimafei Kalinouski <tsimafei.kalinouski@thomsonreuters.com>
2021-08-18 16:27:05 +0300

    feat: add custom directive

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,18 +4,20 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppComponent } from './app.component';
 import { MediaItemComponent } from './components/media-item/media-item.component';
 import { MediaItemListComponent } from './components/media-item-list/media-item-list.component';
+import { FavoriteDirective } from './directives/favorite.directive';
 
 @NgModule({
-  imports: [
-    BrowserModule,
-  ],
-  declarations: [
-    AppComponent,
-    MediaItemComponent,
-    MediaItemListComponent,
-  ],
-  bootstrap: [
-    AppComponent,
-  ],
+	imports: [
+		BrowserModule,
+	],
+	declarations: [
+		AppComponent,
+		MediaItemComponent,
+		MediaItemListComponent,
+		FavoriteDirective,
+	],
+	bootstrap: [
+		AppComponent,
+	],
 })
 export class AppModule {}
