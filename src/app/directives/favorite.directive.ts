59e5c32efbb1f53c90f256680947ded9f06df382
Tsimafei Kalinouski <tsimafei.kalinouski@thomsonreuters.com>
2021-08-18 17:10:17 +0300

    refactor: add types to methods and parameters

--- src/app/directives/favorite.directive.ts
+++ src/app/directives/favorite.directive.ts
@@ -7,14 +7,14 @@ export class FavoriteDirective {
 	@HostBinding('class.is-favorite') isFavorite = true;
 	@HostBinding('class.is-favorite-hovering') hovering = false;
 
-	@HostListener('mouseenter') onMouseEnter() {
+	@HostListener('mouseenter') onMouseEnter(): void {
 		this.hovering = true;
 	}
-	@HostListener('mouseleave') onMouseLeave() {
+	@HostListener('mouseleave') onMouseLeave(): void {
 		this.hovering = false;
 	}
 
-	@Input() set appFavorite(value) {
+	@Input() set appFavorite(value: boolean) {
 		this.isFavorite = value;
 	}
 }
