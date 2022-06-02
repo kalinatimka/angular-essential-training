357e9aef7433d9d0dff6a74da8d51ece9ba902b3
Timofey <kalinata0799@gmail.com>
2022-06-02 17:24:39 +0300

    feat: add http client module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,7 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { ReactiveFormsModule } from '@angular/forms';
+import { HttpClientModule } from '@angular/common/http';
 import { AppComponent } from './app.component';
 import { MediaItemComponent } from './components/media-item/media-item.component';
 import { MediaItemListComponent } from './components/media-item-list/media-item-list.component';
@@ -11,6 +12,7 @@ import { MediaItemFormComponent } from './components/media-item-form/media-item-
 	imports: [
 		BrowserModule,
 		ReactiveFormsModule,
+		HttpClientModule,
 	],
 	declarations: [
 		AppComponent,
