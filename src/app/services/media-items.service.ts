08faf08c9abf6ccc0eafb63eb93a9d00596be787
Timofey <kalinata0799@gmail.com>
2022-06-04 19:53:58 +0300

    feat: add error handling for http requests

--- src/app/services/media-items.service.ts
+++ src/app/services/media-items.service.ts
@@ -29,14 +29,24 @@ export class MediaItemsService {
 		const options = { params: { medium } };
 		return this.http.get<MediaItemResponse>('mediaitems', options).pipe(
 			map((response) => response.mediaItems),
+			catchError(this.handleError),
 		);
 	}
 
 	public addMediaItem(mediaItem: MediaItem): Observable<unknown> {
-		return this.http.post('mediaitems', mediaItem);
+		return this.http.post('mediaitems', mediaItem).pipe(
+			catchError(this.handleError),
+		);
 	}
 
 	public deleteMediaItem(mediaItem: MediaItem): Observable<unknown> {
-		return this.http.delete(`mediaitems/${mediaItem.id}`);
+		return this.http.delete(`mediaitems/${mediaItem.id}`).pipe(
+			catchError(this.handleError),
+		);
+	}
+
+	private handleError(error: HttpErrorResponse) {
+		console.log(error.message);
+		return throwError('A data error occured, please try again.');
 	}
 }
